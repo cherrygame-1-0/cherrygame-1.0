42622396b0d70279b159f44c022c2f9f31f4cebb
smallmoon2 <106749904+smallmoon2@users.noreply.github.com>
2023-01-12 22:53:33 +0900

    총과꿀

---
+++ Assets/Scenes/col.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class col : MonoBehaviour
+{
+	private Transform cube;
+
+	void Start()
+    {
+		print("start");
+    }
+
+	void OnCollisionEnter(Collision other)
+	{
+
+		if (other.gameObject.tag == "object")
+		{
+
+			cube = GameObject.Find("player").transform.Find("gun_grab");
+
+			Destroy(other.gameObject);
+
+			if (other.gameObject.name == "gun")
+			{
+				cube.gameObject.SetActive(false);
+				cube = GameObject.Find("player").transform.Find("gun_grab");
+
+				print("ÃÑ È¹µæ");
+				cube.gameObject.SetActive(true);
+
+			}
+
+			if (other.gameObject.name == "honey")
+            {
+
+				cube.gameObject.SetActive(false);
+				cube = GameObject.Find("player").transform.Find("honey_grab");
+				print("²Ü È¹µæ");
+				cube.gameObject.SetActive(true);
+			}
+
+		}
+
+	}
+
+}
---
+++ Assets/Scenes/shot.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shot : MonoBehaviour
+{
+    public GameObject bullet;
+    public GameObject spot;
+    private Transform cube;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        cube = GameObject.Find("player").transform.Find("gun_grab");
+
+        if (cube.gameObject.activeSelf == true)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                GameObject obj = Instantiate(bullet, spot.transform.position, Quaternion.identity);
+                obj.GetComponent<Rigidbody>().AddForce(spot.transform.forward * 700f);
+                print("น฿ป็");
+                Destroy(obj, 5);
+            }
+        }
+
+    }
+
+}
