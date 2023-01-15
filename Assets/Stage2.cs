9126ebb14a7c43dd299cbf82979fe05c74ef9d21
HyeonSikYun <109672921+HyeonSikYun@users.noreply.github.com>
2023-01-15 22:52:18 +0900

    fdfsdasf

---
+++ Assets/Stage2.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Stage2 : MonoBehaviour
+{
+    public void GameSceneCtrl()
+    {
+        SceneManager.LoadScene("Stage2");
+    }
+}
