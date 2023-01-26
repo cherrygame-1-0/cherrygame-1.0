3e98b393246824b029cc6c68983d0fc6d8b486ca
HyeonSikYun <109672921+HyeonSikYun@users.noreply.github.com>
2023-01-26 22:34:23 +0900

    rrttre

--- Assets/윤현식/NextStage.cs
+++ Assets/윤현식/NextStage.cs
@@ -7,9 +7,25 @@ public class NextStage : MonoBehaviour
 {
     public GameObject NextStageUI;
 
+    public GameObject f_coin;
+    public GameObject s_coin;
+    public GameObject t_coin;
+
     public void Show()
     {
         NextStageUI.SetActive(true); // GameOver 팝업 창을 화면에 표시 시키고
+        if (coindraw.coin >= 1)
+        {
+            f_coin.SetActive(true);
+        }
+        if(coindraw.coin>=2)
+        {
+            s_coin.SetActive(true);
+        }    
+        if(coindraw.coin>=3)
+        {
+            t_coin.SetActive(true);
+        }
     }
 
     public void Goal()
