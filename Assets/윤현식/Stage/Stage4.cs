98272eb16e488e8459ea8319d051f83302f654b7
smallmoon2 <106749904+smallmoon2@users.noreply.github.com>
2023-02-12 22:28:38 +0900

    1234
    1234

---
+++ Assets/윤현식/Stage/Stage4.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Stage4 : MonoBehaviour
+{
+    public void GameSceneCtrl()
+    {
+        SceneManager.LoadScene("Stage4");
+    }
+}
---
+++ Assets/윤현식/Stage/Stage6.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Stage6 : MonoBehaviour
+{
+    public void GameSceneCtrl()
+    {
+        SceneManager.LoadScene("Stage6");
+    }
+}
