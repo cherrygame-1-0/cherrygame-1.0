e44b2cf817377877fe97d713eea8a237cc54e44a
lhs0813 <109675112+lhs0813@users.noreply.github.com>
2023-02-23 20:33:36 +0900

    asdas
    dsad

---
+++ Assets/이현수/Stage/Desert/CameraMove_ST11.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove_ST11 : MonoBehaviour
+{
+    public Transform targetTransform;
+    public Vector3 CameraOffset;
+    Vector3 destination = new Vector3(0.5986536f, 6.771896f, 184.1656f);
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (GameObject.Find("player").GetComponent<Col_ST11>().GoalCheck == true)
+
+
+        {
+
+            transform.position = Vector3.MoveTowards(transform.position, destination, 0.3f);
+            transform.rotation = Quaternion.Euler(30, 180, 0);
+        }
+        else
+        {
+            transform.position = targetTransform.position + CameraOffset;
+        }
+    }
+}
